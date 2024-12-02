cb975c193ba87537eea3226aee34e5a9db2ab64f
AtharvRajurkar <154738411+AtharvRajurkar@users.noreply.github.com>
2024-12-03 03:02:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,19 +16,6 @@ const resizeObserverLoopErr = (err) => {
 
 window.addEventListener("error", resizeObserverLoopErr);
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
-
-// rootElement.render(<React.StrictMode>
-//   <App />
-// </React.StrictMode>);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
